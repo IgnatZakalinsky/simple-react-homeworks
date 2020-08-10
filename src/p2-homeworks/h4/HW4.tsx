7ca416ee8e5f5cb7449ecba867db684d402c45e2
ai73a@yandex.ru <1qazxcvBG>
2020-08-10 16:56:18 +0300

    add super checkbox

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
-import SuperInputText from "./common/SuperInputText/SuperInputText";
+import React, {ChangeEvent, useState} from "react";
+import SuperInputText from "./common/c1-SuperInputText/SuperInputText";
 import s from "./HW4.module.css";
-import SuperButton from "./common/SuperButton/SuperButton";
+import SuperButton from "./common/c2-SuperButton/SuperButton";
+import SuperCheckbox from "./common/c3-SuperCheckbox/SuperCheckbox";
 
 function HW4() {
     const [text, setText] = useState<string>("");
@@ -9,35 +10,53 @@ function HW4() {
         if (error) {
             alert("введите текст...");
         } else {
-            alert(text); // если нет ошибки показать текс
+            alert(text); // если нет ошибки показать текст
         }
     }
     const error = text ? "" : "error";
+    const [checked, setChecked] = useState<boolean>(false);
+    const testOnChange = (e: ChangeEvent<HTMLInputElement>) => setChecked(e.currentTarget.checked);
 
     return (
         <div>
             <hr/>
             homeworks 4
 
-            {/*should work (должно работать)*/}
-            <SuperInputText
-                value={text}
-                onChangeText={setText}
-                onEnter={showAlert}
-                error={error}
-                // className={s.blue} // проверьте, рабоет ли смешивание классов
-            />
-            {/*should work (должно работать)*/}
-            <SuperButton
-                red // пропсу с булевым значением не обязательно указывать true
-                onClick={showAlert}
-            >
-                delete {/*// название кнопки попадёт в children*/}
-            </SuperButton>
+            <div className={s.column}>
+                {/*should work (должно работать)*/}
+                <SuperInputText
+                    value={text}
+                    onChangeText={setText}
+                    onEnter={showAlert}
+                    error={error}
+                    // className={s.blue} // проверьте, рабоет ли смешивание классов
+                />
+
+                {/*should work (должно работать)*/}
+                <SuperButton
+                    red // пропсу с булевым значением не обязательно указывать true
+                    onClick={showAlert}
+                >
+                    delete {/*// название кнопки попадёт в children*/}
+                </SuperButton>
+
+                {/*should work (должно работать)*/}
+                <SuperCheckbox
+                    checked={checked}
+                    onChangeChecked={setChecked}
+                >
+                    some text {/*// этот текст попадёт в children*/}
+                </SuperCheckbox>
+
+                {/*// onChange тоже должен работать*/}
+                <SuperCheckbox checked={checked} onChange={testOnChange}/>
+            </div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperInputText/>*/}
+            {/*<AlternativeSuperButton/>*/}
+            {/*<AlternativeSuperCheckbox/>*/}
             <hr/>
         </div>
     );
