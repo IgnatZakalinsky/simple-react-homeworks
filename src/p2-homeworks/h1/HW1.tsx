e38a64b86929418df6d3dd87f5e1ae7ec2fe87d8
Ramhar <noobocode@gmail.com>
2021-01-08 14:37:08 +0300

    Release 1,2,3,5,6,7,9. All tasks done without difficult border and AlternativeMessages

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Message from './Message'
 
 const messageData = {
     avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
-    name: "Some Name",
-    message: "some text",
+    name: "Valdemar",
+    message: "Hello, my dear friend!",
     time: "22:00",
 };
 
@@ -12,16 +13,12 @@ function HW1() {
         <div>
             <hr/>
             homeworks 1
-
             {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time} />
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeMessage/>*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import MessageClasses from './Message.module.css';
 
-function Message() {
+type messageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: messageType) {
     return (
-        <div>
-
+        <div className={MessageClasses.message}>
+            <img className={MessageClasses.avatar} src={props.avatar} alt="" />
+            <div className={MessageClasses.textArea}>
+                <div className={MessageClasses.name}>{props.name}</div>
+                <div className={MessageClasses.info}>{props.message}</div>
+                <div className={MessageClasses.time}>{props.time}</div>
+            </div>
         </div>
     );
 }
