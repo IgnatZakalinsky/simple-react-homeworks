8dee3231a22c7798291127af9124773450a4ade2
IgnatZakalinsky <ai73a@yandex.ru>
2021-02-07 19:32:55 +0300

    hw 2 refactoring

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -1,35 +1,35 @@
-import React, {useState} from "react";
-import Affairs from "./Affairs";
+import React, {useState} from 'react'
+import Affairs from './Affairs'
 
 // types
-export type AffairPriorityType = any; // need to fix any
-export type AffairType = any; // need to fix any
-export type FilterType = "all" | AffairPriorityType;
+export type AffairPriorityType = any // need to fix any
+export type AffairType = any // need to fix any
+export type FilterType = 'all' | AffairPriorityType
 
 // constants
 const defaultAffairs: any = [ // need to fix any
-    {_id: 1, name: "React", priority: "high"},
-    {_id: 2, name: "anime", priority: "low"},
-    {_id: 3, name: "games", priority: "low"},
-    {_id: 4, name: "work", priority: "high"},
-    {_id: 5, name: "html & css", priority: "middle"},
-];
+    {_id: 1, name: 'React', priority: 'high'},
+    {_id: 2, name: 'anime', priority: 'low'},
+    {_id: 3, name: 'games', priority: 'low'},
+    {_id: 4, name: 'work', priority: 'high'},
+    {_id: 5, name: 'html & css', priority: 'middle'},
+]
 
 // pure helper functions
 export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-    if (filter === "all") return affairs;
-    else return; // need to fix
+    if (filter === 'all') return affairs
+    else return // need to fix
 }
 export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-    return; // need to fix
+    return // need to fix
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs); // need to fix any
-    const [filter, setFilter] = useState<FilterType>("all");
+    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [filter, setFilter] = useState<FilterType>('all')
 
-    const filteredAffairs = filterAffairs(affairs, filter);
-    const deleteAffairCallback = (_id: any) => setAffairs(deleteAffair(affairs, _id)); // need to fix any
+    const filteredAffairs = filterAffairs(affairs, filter)
+    const deleteAffairCallback = (_id: any) => setAffairs(deleteAffair(affairs, _id)) // need to fix any
 
     return (
         <div>
@@ -48,7 +48,7 @@ function HW2() {
             {/*<AlternativeAffairs/>*/}
             <hr/>
         </div>
-    );
+    )
 }
 
-export default HW2;
+export default HW2
--- src/p2-homeworks/h2/tests/deleteAffair.test.tsx
+++ src/p2-homeworks/h2/tests/deleteAffair.test.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import {AffairType, deleteAffair} from "../HW2";
+import React from 'react'
+import {AffairType, deleteAffair} from '../HW2'
 
-let initialState: AffairType[];
+let initialState: AffairType[]
 
 beforeEach(() => {
     initialState = [
-        {_id: 1, name: "React", priority: "high"},
-        {_id: 2, name: "anime", priority: "low"},
-        {_id: 3, name: "games", priority: "low"},
-        {_id: 4, name: "work", priority: "high"},
-        {_id: 5, name: "html & css", priority: "middle"},
-        {_id: 6, name: "porn", priority: "low"},
+        {_id: 1, name: 'React', priority: 'high'},
+        {_id: 2, name: 'anime', priority: 'low'},
+        {_id: 3, name: 'games', priority: 'low'},
+        {_id: 4, name: 'work', priority: 'high'},
+        {_id: 5, name: 'html & css', priority: 'middle'},
+        {_id: 6, name: 'porn', priority: 'low'},
     ]
-});
+})
 
-test("delete 0", () => {
-    const newState = deleteAffair(initialState, 0);
-    expect(newState.length).toBe(6);
-});
-test("delete 1", () => {
-    const newState = deleteAffair(initialState, 1);
-    expect(newState.length).toBe(5);
-});
-test("delete 3", () => {
-    const newState = deleteAffair(initialState, 3);
-    expect(newState.length).toBe(5);
-});
-test("delete 6", () => {
-    const newState = deleteAffair(initialState, 6);
-    expect(newState.length).toBe(5);
-});
-test("delete 7", () => {
-    const newState = deleteAffair(initialState, 7);
-    expect(newState.length).toBe(6);
-});
+test('delete 0', () => {
+    const newState = deleteAffair(initialState, 0)
+    expect(newState.length).toBe(6)
+})
+test('delete 1', () => {
+    const newState = deleteAffair(initialState, 1)
+    expect(newState.length).toBe(5)
+})
+test('delete 3', () => {
+    const newState = deleteAffair(initialState, 3)
+    expect(newState.length).toBe(5)
+})
+test('delete 6', () => {
+    const newState = deleteAffair(initialState, 6)
+    expect(newState.length).toBe(5)
+})
+test('delete 7', () => {
+    const newState = deleteAffair(initialState, 7)
+    expect(newState.length).toBe(6)
+})
