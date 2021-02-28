c2d929f3d02191fc691fe5e0b12da8c476914e58
neko <ai73a@yandex.ru>
2021-02-28 20:02:07 +0300

    Updates

---
+++ precache-manifest.d9935c06489d34ebcd38fcd2164b3d6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40509bcc881b99a269f9ed156df609c6",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "deb08758d8664a56bcc7",
+    "url": "/simple-react-homeworks/static/css/main.f2841dd8.chunk.css"
+  },
+  {
+    "revision": "1523067b32c31da24252",
+    "url": "/simple-react-homeworks/static/js/2.3ce9afbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks/static/js/2.3ce9afbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb08758d8664a56bcc7",
+    "url": "/simple-react-homeworks/static/js/main.b6d843f9.chunk.js"
+  },
+  {
+    "revision": "af4ec8013c5fe87b14da",
+    "url": "/simple-react-homeworks/static/js/runtime-main.f59815ab.js"
+  },
+  {
+    "revision": "7e6e1cf7ef72619dadc9708d5de76631",
+    "url": "/simple-react-homeworks/static/media/cosmos.7e6e1cf7.svg"
+  }
+]);
