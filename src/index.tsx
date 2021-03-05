db67b5b50bfd5ae97cdebf4dec7e619c55353d32
ai73a@yandex.ru <1qazxcvBG>
2021-03-05 21:08:44 +0300

    t2

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
     document.getElementById("root")
 )
 
-//t1
+//t2
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
