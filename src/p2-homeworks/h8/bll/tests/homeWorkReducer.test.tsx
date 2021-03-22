6d372e194aa8dbf9ae005f23f05072604168f4f8
neko <ai73a@yandex.ru>
2021-03-22 12:17:52 +0300

    hw8 refactoring

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import {homeWorkReducer} from "../homeWorkReducer";
+import React from 'react'
+import {homeWorkReducer} from '../homeWorkReducer'
 
-let initialState: any[];
+let initialState: any[] // need to fix any
 
 beforeEach(() => {
     initialState = [
-        {_id: 0, name: "Кот", age: 3},
-        {_id: 1, name: "Александр", age: 66},
-        {_id: 2, name: "Коля", age: 16},
-        {_id: 3, name: "Виктор", age: 44},
-        {_id: 4, name: "Дмитрий", age: 40},
-        {_id: 5, name: "Ирина", age: 55},
+        {_id: 0, name: 'Кот', age: 3},
+        {_id: 1, name: 'Александр', age: 66},
+        {_id: 2, name: 'Коля', age: 16},
+        {_id: 3, name: 'Виктор', age: 44},
+        {_id: 4, name: 'Дмитрий', age: 40},
+        {_id: 5, name: 'Ирина', age: 55},
     ]
-});
+})
 
-test("sort name up", () => {
-    const newState = homeWorkReducer(initialState, {type: "sort", payload: "up"});
+test('sort name up', () => {
+    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
-    console.log(newState);
-    // expect(...).toBe(...);
-});
-test("sort name down", () => {
-    const newState = homeWorkReducer(initialState, {type: "sort", payload: "down"});
-
-
-});
-test("check age 18", () => {
-    const newState = homeWorkReducer(initialState, {type: "check", payload: 18});
+    console.log(newState)
+    // expect(...).toBe(...)
+})
+test('sort name down', () => {
+    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
 
 
-});
+})
+test('check age 18', () => {
+    const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
+
+
+})
