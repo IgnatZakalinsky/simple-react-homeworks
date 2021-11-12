f6b806fb0018439ade6bd2b8254e0919a4f52078
neko <IgnatZakalinsky>
2021-11-12 19:04:30 +0300

    x1

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
 import * as serviceWorker from './serviceWorker'
-
+// sdfgh
 ReactDOM.render(
     <React.StrictMode>
         <App/>
