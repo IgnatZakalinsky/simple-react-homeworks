ce53e224592332a75a05b270c25b7a622a4076e8
neko <IgnatZakalinsky>
2022-03-06 20:21:02 +0300

    test

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Header from './Header'
 import Pages from './Pages'
+import {HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
             <Header/>
 
             <Pages/>
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -8,20 +11,20 @@ export const PATH = {
 function Pages() {
     return (
         <div>
-            {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+            {/*Routes выбирает первый подходящий роут*/}
+            <Routes>
 
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-            {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
+            <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
+            <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 // add routes
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+            <Route path={'/*'} element={<Error404/>}/>
 
-            {/*</Switch>*/}
+            </Routes>
         </div>
     )
 }
