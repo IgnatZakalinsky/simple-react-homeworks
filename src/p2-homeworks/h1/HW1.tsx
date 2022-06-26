61312792f104b1bb328ed42c5c4fa36815c69658
Anastasiia Symvolokova <ansymvolokova@gmail.com>
2022-06-26 14:06:09 +0200

    first commit

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Message from "./Message";
+
+
+
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
+    name: 'Artem',
+    message: 'Npm start нажимал?',
     time: '22:00',
 }
 
@@ -11,16 +15,15 @@ function HW1() {
     return (
         <div>
             <hr/>
-            homeworks 1
-
+            {/*homeworks 1*/}
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
