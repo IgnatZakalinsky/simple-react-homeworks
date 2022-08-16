86f538e0b8a9387bcdec9e749a9b60852fc66214
George Erokhin <Georgii070684@gmail.com>
2022-08-16 14:23:39 +0300

    feat: add first task

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { TMessageData } from './HW1'
+import s from "./Message.module.css"
 
-function Message() {
+
+function Message(props: TMessageData) {
     return (
-        <div>
-
-        </div>
+        <div className={s.wrapper}>
+            <div className={s.item}>
+                <div className={s.itemAvatar} >
+                <img src ={props.avatar} className={s.itemImage} ></img>
+                </div>
+                <div className={s.itemDialog}>
+                    <div className={s.itemName}>
+                        {props.name}
+                    </div>
+                    <div className={s.itemPost}>
+                        {props.message}
+                    </div>
+                    <div className={s.itemTime}> {props.time}</div>
+                </div>
+            </div>
+        </div >
     )
 }
 
