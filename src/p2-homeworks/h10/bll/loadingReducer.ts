912a1b4a8b5b4075c4baadbd3d4838f2850e9f3b
George Erokhin <Georgii070684@gmail.com>
2022-11-08 16:37:37 +0300

    feat: add resolve 11 task(DoubleRange)

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,28 +1,28 @@
- export type StateType = {
-  loading: boolean;
+export type StateType = {
+    loading: boolean;
 };
 type LoadingACType = {
-  type: "ISLOADING";
+    type: "ISLOADING";
 };
 
 const initState = {
-  loading:false,
+    loading: false,
 };
 
 export const loadingReducer = (state = initState, action: LoadingACType): StateType => {
-  // fix any
-  switch (action.type) {
-    case "ISLOADING": {
-      return { ...state, loading: !state.loading };
+    // fix any
+    switch (action.type) {
+        case "ISLOADING": {
+            return {...state, loading: !state.loading};
+        }
+        default:
+            return state;
     }
-    default:
-      return state;
-  }
 };
 
 export const loadingAC = (): LoadingACType =>
-  ({
-    type: "ISLOADING",
-  } as const); // fix any
+    ({
+        type: "ISLOADING",
+    } as const); // fix any
 
 
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
+import {Box, Slider} from "@material-ui/core";
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    onChangeRange?: (value: number[]) => void
+    value?: number[]
     // min, max, step, disable, ...
 }
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
+export const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
         // min, max, step, disable, ...
     }
 ) => {
+    console.log("rest", onChangeRange)
     // сделать самому, можно подключать библиотеки
+    const handleChange = (event: React.ChangeEvent<{}>, value: number | number[]) => {
+        // setValue(newValue as number[]);
+        Array.isArray(value) && onChangeRange && onChangeRange(value)
+    };
+    // function valuetext(value: number) {
+    //     return `${value}°C`;
+    // }
 
     return (
-        <>
-            DoubleRange
-        </>
+        <div style={{width:160}}>
+            <Slider
+                // getAriaLabel={() => 'Temperature range'}
+                value={value || [0, 100]}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                // getAriaValueText={valuetext}
+            />
+        </div>
     )
 }
-
-export default SuperDoubleRange
